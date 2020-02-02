20d1c69b41cd7285eb1932374f2000682cd22d1f
Aitor <aitor.tena@betterconsultants.es>
2020-02-02 16:29:30 +0100

    Logging with ILogger

--- RebelRegistration/Controllers/RebelInfoController.cs
+++ RebelRegistration/Controllers/RebelInfoController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using RebelRegistration.Models;
 
 namespace RebelRegistration.Controllers
@@ -17,150 +18,101 @@ namespace RebelRegistration.Controllers
         private StreamReader _streamReader;                                     // StreamWriter object to write to rebel log
         private readonly string _logPath = "Logs\\RebelLog.txt";                // Rebel log path
         private readonly string _dateFormat = "yyyy'-'MM'-'dd' 'HH':'mm':'ss";  // Date format for Rebel log
+        private readonly ILogger _logger;                                       // Logger
+
+        public RebelInfoController(ILogger<RebelInfoController> logger)
+        {
+            _logger = logger;
+        }
 
         // GET: api/RebelInfo
-        //[HttpGet]
-        //public ActionResult<IEnumerable<string>> Get()
-        //{
-        //    try
-        //    {
-        //        _streamReader = new StreamReader(_logPath);
-        //    }
-        //    catch (DirectoryNotFoundException ex)
-        //    {
-        //        return BadRequest($"An error ocurred retrieving rebel log entries.\nError: {ex.Message}.\nTry again later.");
-        //    }
-        //    catch (IOException ex)
-        //    {
-        //        return BadRequest($"An error ocurred retrieving rebel log entries.\nError: {ex.Message}.\nTry again later.");
-        //    }
-
-        //    // TODO - Read all entries Async
-
-        //    List<string> l_logEntries = new List<string>();
-
-        //    while (!_streamReader.EndOfStream)
-        //    {
-        //        l_logEntries.Add(_streamReader.ReadLine());
-        //    }
-
-        //    _streamReader.Dispose();
-
-        //    return l_logEntries.ToArray();
-        //}
         [HttpGet]
         public async Task<ActionResult<IEnumerable<string>>> Get()
         {
             try
             {
                 _streamReader = new StreamReader(_logPath);
-            }
-            catch (DirectoryNotFoundException ex)
-            {
-                return BadRequest($"An error ocurred retrieving rebel log entries.\nError: {ex.Message}.\nTry again later.");
-            }
-            catch (IOException ex)
-            {
-                return BadRequest($"An error ocurred retrieving rebel log entries.\nError: {ex.Message}.\nTry again later.");
-            }
 
-            return await Task.Run(() =>
-            {
-                List<string> l_logEntries = new List<string>();
-
-                while (!_streamReader.EndOfStream)
+                return await Task.Run(() =>
                 {
-                    l_logEntries.Add(_streamReader.ReadLine());
+                    List<string> l_logEntries = new List<string>();
+
+                    while (!_streamReader.EndOfStream)
+                    {
+                        l_logEntries.Add(_streamReader.ReadLine());
+                    }
+
+                    _streamReader.Dispose();
+
+                    if (l_logEntries.Count > 0)
+                    {
+                        _logger.LogDebug($"Returned {l_logEntries.Count} log entries");
+
+                        return l_logEntries.ToArray();
+                    }
+                    else
+                    {
+                        _logger.LogWarning($"No rebel logs found");
+
+                        return new string[] { "Rebel logs not found. Keep searching for the enemies of the Republic!" };
+                    }
                 }
-
-                _streamReader.Dispose();
-
-                return l_logEntries.ToArray();
-            }
             );
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception found getting rebel logs: exception {ex.Message}");
+
+                return BadRequest($"An error ocurred retrieving rebel log entries.\nError: {ex.Message}.\nTry again later.");
+            }
+
+            
         }
 
-
         // GET: api/RebelInfo/Aitor
-        //[HttpGet("{name}", Name = "Get")]
-        //public ActionResult<IEnumerable<string>> Get(string name)
-        //{
-        //    try
-        //    {
-        //        _streamReader = new StreamReader(_logPath);
-        //    }
-        //    catch (DirectoryNotFoundException ex)
-        //    {
-        //        return BadRequest($"An error ocurred retrieving rebel log entries.\nError: {ex.Message}.\nTry again later.");
-        //    }
-        //    catch (IOException ex)
-        //    {
-        //        return BadRequest($"An error ocurred retrieving rebel log entries.\nError: {ex.Message}.\nTry again later.");
-        //    }
-
-        //    List<string> l_logEntries = new List<string>();
-
-        //    while (!_streamReader.EndOfStream)
-        //    {
-        //        string l_logEntry = _streamReader.ReadLine();
-        //        int l_pos = l_logEntry.IndexOf(name);
-
-        //        if (l_pos != -1)
-        //            l_logEntries.Add(l_logEntry);
-        //    }
-
-        //    _streamReader.Dispose();
-
-        //    if (l_logEntries.Count > 0)
-        //    {
-        //        return l_logEntries.ToArray();
-        //    }
-        //    else
-        //    {
-        //        return new string[] { "Rebel not found in log. Keep searching for the enemies of the Republic!" };
-        //    }
-        //}
         [HttpGet("{name}", Name = "Get")]
         public async Task<ActionResult<IEnumerable<string>>> Get(string name)
         {
             try
             {
                 _streamReader = new StreamReader(_logPath);
-            }
-            catch (DirectoryNotFoundException ex)
-            {
-                return BadRequest($"An error ocurred retrieving rebel log entries.\nError: {ex.Message}.\nTry again later.");
-            }
-            catch (IOException ex)
-            {
-                return BadRequest($"An error ocurred retrieving rebel log entries.\nError: {ex.Message}.\nTry again later.");
-            }
 
-            return await Task.Run(() =>
-            {
-                List<string> l_logEntries = new List<string>();
-
-                while (!_streamReader.EndOfStream)
+                return await Task.Run(() =>
                 {
-                    string l_logEntry = _streamReader.ReadLine();
-                    int l_pos = l_logEntry.IndexOf(name);
+                    List<string> l_logEntries = new List<string>();
 
-                    if (l_pos != -1)
-                        l_logEntries.Add(l_logEntry);
-                }
+                    while (!_streamReader.EndOfStream)
+                    {
+                        string l_logEntry = _streamReader.ReadLine();
+                        int l_pos = l_logEntry.IndexOf(name);
 
-                _streamReader.Dispose();
+                        if (l_pos != -1)
+                            l_logEntries.Add(l_logEntry);
+                    }
 
-                if (l_logEntries.Count > 0)
-                {
-                    return l_logEntries.ToArray();
+                    _streamReader.Dispose();
+
+                    if (l_logEntries.Count > 0)
+                    {
+                        _logger.LogDebug($"Rebel logs found with name {name}");
+
+                        return l_logEntries.ToArray();
+                    }
+                    else
+                    {
+                        _logger.LogWarning($"No rebel log found with name {name}");
+
+                        return new string[] { "Rebel not found in log. Keep searching for the enemies of the Republic!" };
+                    }
                 }
-                else
-                {
-                    return new string[] { "Rebel not found in log. Keep searching for the enemies of the Republic!" };
-                }
-            }
             );
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception found getting rebel info with name {name}: exception {ex.Message}");
+
+                return BadRequest($"An error ocurred retrieving rebel log entries.\nError: {ex.Message}.\nTry again later.");
+            }
         }
 
         // POST: api/RebelInfo
@@ -171,24 +123,24 @@ namespace RebelRegistration.Controllers
             try
             {
                 _streamWriter = new StreamWriter(_logPath, true);
+
+                string l_dateTime = DateTime.Now.ToString(_dateFormat);
+
+                // Write to Rebel log
+                _streamWriter.WriteLine($"Rebel {rebelInfo.Name} on {rebelInfo.Planet} at {l_dateTime}");
+                _streamWriter.Flush();
+                _streamWriter.Dispose();
+
+                _logger.LogDebug("Logged rebel info successfully", rebelInfo);
+
+                return CreatedAtAction("Get", $"Rebel {rebelInfo.Name} registered on planet {rebelInfo.Planet} at {l_dateTime}");
             }
-            catch(DirectoryNotFoundException ex)
+            catch(Exception ex)
             {
+                _logger.LogError($"Exception found posting rebel info: exception {ex.Message}");
+
                 return BadRequest($"An error ocurred during the rebel registration.\nError: {ex.Message}.\nTry again later, don't let the rebel get away!");
             }
-            catch(IOException ex)
-            {
-                return BadRequest($"An error ocurred during the rebel registration.\nError: {ex.Message}.\nTry again later, don't let the rebel get away!");
-            }
-
-            string l_dateTime = DateTime.Now.ToString(_dateFormat);
-
-            // Write to Rebel log
-            _streamWriter.WriteLine($"Rebel {rebelInfo.Name} on {rebelInfo.Planet} at {l_dateTime}");
-            _streamWriter.Flush();
-            _streamWriter.Dispose();
-
-            return CreatedAtAction("Get", $"Rebel {rebelInfo.Name} registered on planet {rebelInfo.Planet} at {l_dateTime}");
         }
 
         // PUT: api/RebelInfo/5
--- RebelRegistration/Startup.cs
+++ RebelRegistration/Startup.cs
@@ -28,7 +28,8 @@ namespace RebelRegistration
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -36,6 +37,9 @@ namespace RebelRegistration
             }
 
             app.UseMvc();
+
+            // Application log creation (Serilog)
+            loggerFactory.AddFile("Logs/Log_{Date}.txt", LogLevel.Debug);
         }
     }
 }
