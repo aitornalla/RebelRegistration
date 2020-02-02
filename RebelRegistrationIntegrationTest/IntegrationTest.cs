5aa6d2497ae5ea91309eb5606a8214470c1b492e
Aitor <aitor.tena@betterconsultants.es>
2020-02-02 19:36:51 +0100

    Integration test init

---
+++ RebelRegistrationIntegrationTest/IntegrationTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using RebelRegistration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RebelRegistrationIntegrationTest
+{
+    public class IntegrationTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public IntegrationTest(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        public async Task GetHttpRequest(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            Assert.True(response.IsSuccessStatusCode);
+        }
+    }
+}
